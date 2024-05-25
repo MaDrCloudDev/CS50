91d5c5309e3e867ff0eb2f4c7cd68fef25ef0059
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-05-24 23:18:44 -0700

    strings as pointers 40
    strings as pointers 40

--- week4/addresses.c
+++ week4/addresses.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
+// int main(void) {
+//     int n = 50;
+//     int *p = &n;
+//     printf("%i\n", *p);
+// }
+
 int main(void) {
-    int n = 50;
-    int *p = &n;
-    printf("%i\n", *p);
+    string s = "HI!";
+    printf("%p\n", s);
+    printf("%p\n", s+1);
+    printf("%p\n", s+2);
 }
