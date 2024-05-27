fcbadb047796399fb89a0dcd93c78878bcc34718
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-05-27 09:35:17 -0700

    week 4 53

--- week4/addresses.c
+++ week4/addresses.c
@@ -8,9 +8,28 @@
 //     printf("%i\n", *p);
 // }
 
+// int main(void) {
+//     string s = "HI!";
+//     printf("%p\n", s);
+//     printf("%p\n", s+1);
+//     printf("%p\n", s+2);
+// }
+
+// int main(void) {
+//     char *s = "HI!";
+//     printf("%s\n", s);
+// }
+
+// int main(void) {
+//     char *s = "HI!";
+//     printf("%c", s[0]);
+//     printf("%c", s[1]);
+//     printf("%c\n", s[2]);
+// }
+
 int main(void) {
-    string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", s+1);
-    printf("%p\n", s+2);
+    char *s = "HI!";
+    printf("%c", *s);
+    printf("%c", *(s+1));
+    printf("%c\n", *(s+2));
 }
