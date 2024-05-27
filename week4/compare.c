66df2fa8899c963bc5604fbd447a7cbed5c7d15b
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-05-27 12:29:27 -0700

    updates, up to 1:17
    updates, up to 1:17

--- week4/compare.c
+++ week4/compare.c
@@ -2,13 +2,46 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-    int i = get_int("i: ");
-    int j = get_int("j: ");
+// int main(void) {
+//     int i = get_int("i: ");
+//     int j = get_int("j: ");
 
-    if (i == j) {
+//     if (i == j) {
+//         printf("Same\n");
+//     } else {
+//         printf("Different\n");
+//     }
+// }
+
+// int main(void) {
+//     string s = get_string("s: ");
+//     string t = get_string("t: ");
+
+//     if (s == t) {
+//         printf("Same\n");
+//     } else {
+//         printf("Different\n");
+//     }
+// } // doesn't work, because you have to do this:
+
+// int main(void) {
+//     char *s = get_string("s: ");
+//     char *t = get_string("t: ");
+
+//     if (s == t) {
+//         printf("Same\n");
+//     } else {
+//         printf("Different\n");
+//     }
+// } // doesn't work because of how memory is pointed to, and strings are duplicated.
+
+int main(void) {
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+
+    if (strcmp(s, t) == 0) {
         printf("Same\n");
     } else {
         printf("Different\n");
     }
-}
+} // works
---
+++ week4/copy.c
@@ -0,0 +1,56 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// int main(void) {
+//     string s = get_string("s: ");
+//     string t = s;
+
+//     t[0] = toupper(t[0]);
+
+//     printf("%s\n", s);
+//     printf("%s\n", t);
+// }
+
+// int main(void) {
+//     char *s = get_string("s: ");
+//     char *t = malloc(strlen(s) + 1);
+
+//     for (int i = 0, n = strlen(s); i <= n; i++) {
+//         t[i] = s[i];
+//     }
+
+//     if (strlen(t) > 0) {
+//         t[0] = toupper(t[0]);
+//     }
+
+//     printf("%s\n", s);
+//     printf("%s\n", t);
+// } // but a better convention is:
+
+int main(void) {
+    char *s = get_string("s: ");
+    char *t = malloc(strlen(s) + 1);
+    if (t == NULL) {
+        return 1;
+    }  // we should do this. It's a convention, still used. If you're out
+    // of memory, the thing wont work.
+
+    strcopy(t, s);  // to form a copy of s into t. including the ending null character. But let's also look at free.
+
+    for (int i = 0, n = strlen(s); i <= n; i++) {
+        t[i] = s[i];
+    }
+
+    if (strlen(t) > 0) {
+        t[0] = toupper(t[0]);
+    }
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+
+    free(t);
+    return 0;
+}
