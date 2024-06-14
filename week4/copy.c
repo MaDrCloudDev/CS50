ab5e2a59668b03ad11ccfab8205d4db95de12dbe
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-06-13 23:18:39 -0700

    fix typo, prepping for new fedora install
    fix typo, prepping for new fedora install

--- week4/copy.c
+++ week4/copy.c
@@ -38,7 +38,7 @@ int main(void) {
     }  // we should do this. It's a convention, still used. If you're out
     // of memory, the thing wont work.
 
-    strcopy(t, s);  // to form a copy of s into t. including the ending null character. But let's also look at free.
+    strcpy(t, s);  // to form a copy of s into t. including the ending null character. But let's also look at free.
 
     for (int i = 0, n = strlen(s); i <= n; i++) {
         t[i] = s[i];
