380491456b76ed20ed0ef76892fe3cf337d1b0b6
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-07-06 12:18:26 -0700

    start week 5
    start week 5

--- problem_set_4/pdf.c
+++ problem_set_4/pdf.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
+#include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
-    fread(buffer, 4, 1, "/hi.txt"){
-        return } printf("a is %i, b is %i\n", a, b);
+int main(int argc, string argv[]) {
+    string  filename = argv[1];
+    FILE   *f = fopen(filename, "rb");
+    uint8_t buffer[4];  // uint8_t is only a single byte big
+    int     blocks_read = fread(buffer, 1, 4, f);
+
+    for (int i = 0; i < 4; i++) {
+        printf("%i\n", buffer[i]);  // prints 37, 80, 68, 70
+    }
+    printf("Blocks read: %i\n", blocks_read);
+    fclose(f);
 }
